61fcbbecc46ca5373dbf27b3cdedadf01b5f8c0c
alvizek <avizek@flowwest.com>
2022-10-21 08:53:54 -0700

    prepping knights landing edi package

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -1,4 +1,89 @@
 library(tidyverse)
+library(knitr)
+library(lubridate)
+library(googleCloudStorageR)
+library(Hmisc)
 
+gcs_auth(json_file = Sys.getenv("GCS_AUTH_FILE"))
+gcs_global_bucket(bucket = Sys.getenv("GCS_DEFAULT_BUCKET"))
 
-# save cleaned data to `data/`
+gcs_get_object(object_name = "rst/CAMP/knights_landing/CAMP_Knights.mdb",
+               bucket = gcs_get_global_bucket(),
+               saveToDisk = here::here("data-raw","CAMP.mdb"),
+               overwrite = TRUE)
+knights_camp <- (here::here("data-raw", "CAMP.mdb"))
+
+catch_raw <- mdb.get(knights_camp, tables = "CatchRaw")
+trap_visit <- mdb.get(knights_camp, tables = "TrapVisit") %>%
+  mutate(visitTime = as.POSIXct(visitTime),
+         visitTime2 = as.POSIXct(visitTime2))
+site_lu <- mdb.get(knights_camp, "Site") %>%
+  select(siteName, siteID)
+subsite_lu <- mdb.get(knights_camp, "SubSite") %>%
+  select(subSiteName, subSiteID, siteID) %>%
+  filter(subSiteName != "N/A")
+release <- mdb.get(knights_camp, tables = "Release") %>%
+  mutate(releaseTime = as.POSIXct(releaseTime))
+release_fish <- mdb.get(knights_camp, tables = "ReleaseFish")
+mark_applied <- mdb.get(knights_camp, tables = "MarkApplied")
+mark <-  mdb.get(knights_camp, tables = "MarkExisting")
+environmental <- mdb.get(knights_camp, tables = "EnvDataRaw")
+
+# Format trap table for EDI
+trap_visit_format <- trap_visit %>%
+  select(projectDescriptionID, trapVisitID, trapPositionID, visitTime, visitTime2,
+         visitTypeID, fishProcessedID, inThalwegID, trapFunctioningID, counterAtStart,
+         counterAtEnd, rpmRevolutionsAtStart, rpmSecondsAtStart, rpmRevolutionsAtEnd,
+         rpmSecondsAtEnd, halfConeID, includeCatchID, debrisVolumeCatID, debrisVolume,
+         debrisVolumeUnits) %>%
+  left_join(subsite_lu, by = c("trapPositionID" = "subSiteID")) %>%
+  select(-subSiteName) %>%
+  relocate(siteID, .before = trapPositionID)
+
+write_csv(trap_visit_format, here::here("data", "trap.csv"))
+
+# Format catch table for EDI
+catch_format <- catch_raw %>%
+  select(projectDescriptionID, catchRawID, trapVisitID, taxonID, atCaptureRunID,
+         atCaptureRunMethodID, finalRunID, finalRunMethodID, fishOriginID,
+         lifeStageID, forkLength, totalLength, weight, n, randomID, actualCountID,
+         releaseID, mortID) %>%
+  left_join(trap_visit_format %>%
+              select(projectDescriptionID, trapVisitID, visitTime, visitTime2, visitTypeID, siteID, trapPositionID),
+            by = c("trapVisitID" = "trapVisitID", "projectDescriptionID" = "projectDescriptionID")) %>%
+  relocate(releaseID, .before = taxonID)
+
+write_csv(catch_format, here::here("data", "catch.csv"))
+
+# Format release table for EDI
+release_format <- release %>%
+  select(projectDescriptionID, releaseID, releasePurposeID, markedTaxonID,
+         markedRunID, markedLifeStageID, markedFishOriginID, sourceOfFishSiteID,
+         releaseSiteID, releaseSubSiteID, nMortWhileHandling, nMortAtCheck,
+         nReleased, releaseTime, releaseLightConditionID,
+         testDays, includeTestID) %>%
+  left_join(mark_applied %>%
+              select(projectDescriptionID, releaseID, appliedMarkTypeID, appliedMarkColorID, appliedMarkPositionID, appliedMarkCode),
+            by = c("projectDescriptionID" = "projectDescriptionID", "releaseID" = "releaseID"))
+write_csv(release_format, here::here("data", "release.csv"))
+
+# Format release fish table for EDI
+release_fish_format <- release_fish %>%
+  select(projectDescriptionID, releaseFishID, releaseID, nMarked, forkLength, weight)
+
+write_csv(release_fish_format, here::here("data", "release_fish.csv"))
+
+# Format for mark existing table for EDI
+mark_existing_format <- mark %>%
+  select(projectDescriptionID, catchRawID, markExistingID, markTypeID, markColorID, markPositionID, markCode)
+write_csv(mark_existing_format, here::here("data", "mark_existing.csv"))
+
+# Format for environmental table for EDI
+environmental_format <- environmental %>%
+  select(projectDescriptionID, envDataRawID, trapVisitID, discharge, dischargeUnitID, dischargeSampleGearID, waterVel, waterVelUnitID,
+         waterVelSampleGearID, waterTemp, waterTempUnitID, waterTempSampleGearID, lightPenetration, lightPenetrationUnitID, lightPenetrationSampleGearID,
+         turbidity, turbidityUnitID, turbiditySampleGearID) %>%
+  left_join(trap_visit_format %>%
+              select(projectDescriptionID, trapVisitID, visitTime, visitTime2, visitTypeID, siteID, trapPositionID),
+            by = c("trapVisitID" = "trapVisitID", "projectDescriptionID" = "projectDescriptionID"))
+write_csv(environmental_format, here::here("data", "environmental.csv"))
--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -1,5 +1,87 @@
-library(EDIutils)
+library(EMLaide)
 library(tidyverse)
+library(readxl)
+library(EML)
 
-
+datatable_metadata <-
+  dplyr::tibble(filepath = c("data/environmental.csv",
+                             "data/catch.csv",
+                             "data/mark_existing.csv",
+                             "data/release.csv",
+                             "data/release_fish.csv",
+                             "data/trap.csv"),
+                attribute_info = c("data-raw/metadata/camp_environmental_metadata.xlsx",
+                                   "data-raw/metadata/camp_catch_metadata.xlsx",
+                                   "data-raw/metadata/camp_markexisting_metadata.xlsx",
+                                   "data-raw/metadata/camp_release_metadata.xlsx",
+                                   "data-raw/metadata/camp_releasefish_metadata.xlsx",
+                                   "data-raw/metadata/camp_trap_metadata.xlsx"),
+                datatable_description = c("Environmental covariates",
+                                          "Daily catch",
+                                          "Existing marks on catch",
+                                          "Release trial summary",
+                                          "Release fish measurements",
+                                          "Daily trap operations"),
+                datatable_url = paste0("https://raw.githubusercontent.com/FlowWest/jpe-knights-edi/main/data/",
+                                       c("environmental.csv",
+                                         "catch.csv",
+                                         "mark_existing.csv",
+                                         "release.csv",
+                                         "release_fish.csv",
+                                         "trap.csv")))
 # save cleaned data to `data/`
+excel_path <- "data-raw/metadata/camp_metadata.xlsx"
+sheets <- readxl::excel_sheets(excel_path)
+metadata <- lapply(sheets, function(x) readxl::read_excel(excel_path, sheet = x))
+names(metadata) <- sheets
+
+abstract_docx <- "data-raw/metadata/abstract.docx"
+methods_docx <- "data-raw/metadata/method.docx"
+
+#edi_number <- reserve_edi_id(user_id = Sys.getenv("EDI_USER_ID"), password = Sys.getenv("EDI_PASSWORD"))
+edi_number <- "edi.1243.1"
+
+dataset <- list() %>%
+  add_pub_date() %>%
+  add_title(metadata$title) %>%
+  add_personnel(metadata$personnel) %>%
+  add_keyword_set(metadata$keyword_set) %>%
+  add_abstract(abstract_docx) %>%
+  add_license(metadata$license) %>%
+  add_method(methods_docx) %>%
+  add_maintenance(metadata$maintenance) %>%
+  add_project(metadata$funding) %>%
+  add_coverage(metadata$coverage, metadata$taxonomic_coverage) %>%
+  add_datatable(datatable_metadata)
+
+# GO through and check on all units
+# custom_units <- data.frame(id = c("number of fish", "rotations per minute", "rotations", "nephelometric turbidity units", "day"),
+#                            unitType = c("density", "dimensionless", "dimensionless", "dimensionless", "dimensionless"),
+#                            parentSI = c(NA, NA, NA, NA, NA),
+#                            multiplierToSI = c(NA, NA, NA, NA, NA),
+#                            description = c("Fish density in the enclosure, number of fish in total enclosure space",
+#                                            "Number of trap rotations in one minute",
+#                                            "Total rotations",
+#                                            "Nephelometric turbidity units, common unit for measuring turbidity",
+#                                            "The day sampling occured"))
+
+# unitList <- EML::set_unitList(custom_units)
+
+eml <- list(packageId = edi_number,
+            system = "EDI",
+            access = add_access(),
+            dataset = dataset
+            # additionalMetadata = list(metadata = list(unitList = unitList))
+)
+
+EML::write_eml(eml, "edi.1243.1.xml")
+#EML::eml_validate("edi.1243.1.xml")
+
+# EMLaide::evaluate_edi_package(Sys.getenv("user_ID"), Sys.getenv("password"), "edi.1047.1.xml")
+# EMLaide::upload_edi_package(Sys.getenv("user_ID"), Sys.getenv("password"), "edi.1047.1.xml")
+# doc <- read_xml("edi.1243.1.xml")
+# edi_number<- data.frame(edi_number = doc %>% xml_attr("packageId"))
+# update_number <- edi_number %>%
+#   separate(edi_number, c("edi","package","version"), "\\.") %>%
+#   mutate(version = as.numeric(version) + 1)
+# edi_number <- paste0(update_number$edi, ".", update_number$package, ".", update_number$version)
