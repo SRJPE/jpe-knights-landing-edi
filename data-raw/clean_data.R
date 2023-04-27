16a078cc53ad889df6baa92260d2babd7e39655d
liz-fw <lstebbins@flowwest.com>
2023-04-27 15:49:17 -0700

    reads in files from database sent by Jeanine week of 4-27-2023

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -4,86 +4,117 @@ library(lubridate)
 library(googleCloudStorageR)
 library(Hmisc)
 
-gcs_auth(json_file = Sys.getenv("GCS_AUTH_FILE"))
-gcs_global_bucket(bucket = Sys.getenv("GCS_DEFAULT_BUCKET"))
 
-gcs_get_object(object_name = "rst/CAMP/knights_landing/CAMP_Knights.mdb",
-               bucket = gcs_get_global_bucket(),
-               saveToDisk = here::here("data-raw","CAMP.mdb"),
-               overwrite = TRUE)
-knights_camp <- (here::here("data-raw", "CAMP.mdb"))
+# read in db --------------------------------------------------------------
 
-catch_raw <- mdb.get(knights_camp, tables = "CatchRaw")
-trap_visit <- mdb.get(knights_camp, tables = "TrapVisit") %>%
-  mutate(visitTime = as.POSIXct(visitTime),
-         visitTime2 = as.POSIXct(visitTime2))
-site_lu <- mdb.get(knights_camp, "Site") %>%
-  select(siteName, siteID)
-subsite_lu <- mdb.get(knights_camp, "SubSite") %>%
-  select(subSiteName, subSiteID, siteID) %>%
-  filter(subSiteName != "N/A")
-release <- mdb.get(knights_camp, tables = "Release") %>%
-  mutate(releaseTime = as.POSIXct(releaseTime))
-release_fish <- mdb.get(knights_camp, tables = "ReleaseFish")
-mark_applied <- mdb.get(knights_camp, tables = "MarkApplied")
-mark <-  mdb.get(knights_camp, tables = "MarkExisting")
-environmental <- mdb.get(knights_camp, tables = "EnvDataRaw")
+mdb.get(here::here("data-raw", "CAMP.mdb"), tables = TRUE)
 
-# Format trap table for EDI
-trap_visit_format <- trap_visit %>%
+catch_raw <- mdb.get(here::here("data-raw", "CAMP.mdb"), "CatchRaw") |>
+  glimpse()
+
+trap_raw <- mdb.get(here::here("data-raw", "CAMP.mdb"), "TrapVisit") |>
+  glimpse()
+
+release_raw <- mdb.get(here::here("data-raw", "CAMP.mdb"), "Release") |>
+  mutate(releaseTime = as.POSIXct(releaseTime)) |>
+  glimpse()
+
+releasefish_raw <- mdb.get(here::here("data-raw", "CAMP.mdb"), "ReleaseFish") |>
+  glimpse()
+
+# lookups
+site_lu <- mdb.get(here::here("data-raw", "CAMP.mdb"), "Site") |>
+  select(siteName, siteID) |>  glimpse()
+subsite_lu <- mdb.get(here::here("data-raw", "CAMP.mdb"), "SubSite") |>
+  select(subSiteName, subSiteID, siteID) |>
+  filter(subSiteName != "N/A") |>
+  glimpse()
+
+# other
+mark_applied <- mdb.get(here::here("data-raw", "CAMP.mdb"), "MarkApplied")
+mark_existing_raw <- mdb.get(here::here("data-raw", "CAMP.mdb"), "MarkExisting")
+environmental_raw <- mdb.get(here::here("data-raw", "CAMP.mdb"), "EnvDataRaw")
+
+
+# clean up and format data tables -----------------------------------------
+
+# TODO debrisVolume and debrisVolumeUnits are NAs
+# TODO do we want to keep includeCatchID?
+# TODO what is debrisVolumeCatID?
+trap <- trap_raw |>
   select(projectDescriptionID, trapVisitID, trapPositionID, visitTime, visitTime2,
          visitTypeID, fishProcessedID, inThalwegID, trapFunctioningID, counterAtStart,
          counterAtEnd, rpmRevolutionsAtStart, rpmSecondsAtStart, rpmRevolutionsAtEnd,
          rpmSecondsAtEnd, halfConeID, includeCatchID, debrisVolumeCatID, debrisVolume,
-         debrisVolumeUnits) %>%
-  left_join(subsite_lu, by = c("trapPositionID" = "subSiteID")) %>%
-  select(-subSiteName) %>%
-  relocate(siteID, .before = trapPositionID)
+         debrisVolumeUnits) |>
+  left_join(subsite_lu, by = c("trapPositionID" = "subSiteID")) |>
+  select(-subSiteName) |>
+  mutate(visitTime = as.POSIXct(visitTime),
+         visitTime2 = as.POSIXct(visitTime2)) |>
+  relocate(siteID, .before = trapPositionID) |>
+  glimpse()
 
-write_csv(trap_visit_format, here::here("data", "trap.csv"))
-
-# Format catch table for EDI
-catch_format <- catch_raw %>%
+# TODO what is randomID?
+# TODO do we want to keep mortID?
+# TODO do we want to keep actualCountID?
+# TODO do we want to use lookup tables to get character values for mortID,
+# fishOriginID, finalRunMethodID, finalRunID, atCaptureRunID, atCaptureRunMethodID,
+# lifestageID?
+catch <- catch_raw |>
   select(projectDescriptionID, catchRawID, trapVisitID, taxonID, atCaptureRunID,
          atCaptureRunMethodID, finalRunID, finalRunMethodID, fishOriginID,
          lifeStageID, forkLength, totalLength, weight, n, randomID, actualCountID,
-         releaseID, mortID) %>%
-  left_join(trap_visit_format %>%
-              select(projectDescriptionID, trapVisitID, visitTime, visitTime2, visitTypeID, siteID, trapPositionID),
-            by = c("trapVisitID" = "trapVisitID", "projectDescriptionID" = "projectDescriptionID")) %>%
-  relocate(releaseID, .before = taxonID)
+         releaseID, mortID) |>
+  left_join(trap |>
+              select(projectDescriptionID, trapVisitID, visitTime,
+                     visitTime2, visitTypeID, siteID, trapPositionID),
+            by = c("trapVisitID", "projectDescriptionID")) |>
+  relocate(releaseID, .before = taxonID) |>
+  glimpse()
 
-write_csv(catch_format, here::here("data", "catch.csv"))
-
-# Format release table for EDI
-release_format <- release %>%
+# TODO same question as catch - which IDs do we want character values for (i.e. pull in lookup tables),
+# and which are ok to leave as IDs?
+# TODO appliedMarkCode is empty right now
+# TODO keep nMortAtCheck and nMortWhileHandling?
+release <- release_raw |>
   select(projectDescriptionID, releaseID, releasePurposeID, markedTaxonID,
          markedRunID, markedLifeStageID, markedFishOriginID, sourceOfFishSiteID,
          releaseSiteID, releaseSubSiteID, nMortWhileHandling, nMortAtCheck,
          nReleased, releaseTime, releaseLightConditionID,
-         testDays, includeTestID) %>%
-  left_join(mark_applied %>%
-              select(projectDescriptionID, releaseID, appliedMarkTypeID, appliedMarkColorID, appliedMarkPositionID, appliedMarkCode),
-            by = c("projectDescriptionID" = "projectDescriptionID", "releaseID" = "releaseID"))
-write_csv(release_format, here::here("data", "release.csv"))
+         testDays, includeTestID) |>
+  left_join(mark_applied |>
+              select(projectDescriptionID, releaseID, appliedMarkTypeID,
+                     appliedMarkColorID, appliedMarkPositionID, appliedMarkCode),
+            by = c("projectDescriptionID", "releaseID")) |>
+  glimpse()
 
-# Format release fish table for EDI
-release_fish_format <- release_fish %>%
-  select(projectDescriptionID, releaseFishID, releaseID, nMarked, forkLength, weight)
+releasefish <- releasefish_raw |>
+  select(projectDescriptionID, releaseFishID, releaseID, nMarked, forkLength, weight) |>
+  glimpse()
 
-write_csv(release_fish_format, here::here("data", "release_fish.csv"))
+# other tables
+# TODO do we still want these?
+mark_existing <- mark_existing_raw |>
+  select(projectDescriptionID, catchRawID, markExistingID, markTypeID,
+         markColorID, markPositionID, markCode) |>
+  mutate(markCode = ifelse(markCode == "", NA_character_, markCode)) |>
+  glimpse()
 
-# Format for mark existing table for EDI
-mark_existing_format <- mark %>%
-  select(projectDescriptionID, catchRawID, markExistingID, markTypeID, markColorID, markPositionID, markCode)
-write_csv(mark_existing_format, here::here("data", "mark_existing.csv"))
-
-# Format for environmental table for EDI
-environmental_format <- environmental %>%
+environmental <- environmental_raw |>
   select(projectDescriptionID, envDataRawID, trapVisitID, discharge, dischargeUnitID, dischargeSampleGearID, waterVel, waterVelUnitID,
          waterVelSampleGearID, waterTemp, waterTempUnitID, waterTempSampleGearID, lightPenetration, lightPenetrationUnitID, lightPenetrationSampleGearID,
-         turbidity, turbidityUnitID, turbiditySampleGearID) %>%
-  left_join(trap_visit_format %>%
-              select(projectDescriptionID, trapVisitID, visitTime, visitTime2, visitTypeID, siteID, trapPositionID),
-            by = c("trapVisitID" = "trapVisitID", "projectDescriptionID" = "projectDescriptionID"))
-write_csv(environmental_format, here::here("data", "environmental.csv"))
+         turbidity, turbidityUnitID, turbiditySampleGearID) |>
+  left_join(trap |>
+              select(projectDescriptionID, trapVisitID, visitTime,
+                     visitTime2, visitTypeID, siteID, trapPositionID),
+            by = c("trapVisitID", "projectDescriptionID")) |>
+  glimpse()
+
+# write clean tables ------------------------------------------------------
+
+write_csv(trap, here::here("data", "trap.csv"))
+write_csv(catch, here::here("data", "catch.csv"))
+write_csv(release, here::here("data", "release.csv"))
+write_csv(releasefish, here::here("data", "releasefish.csv"))
+write_csv(mark_existing, here::here("data", "markexisting.csv"))
+write_csv(environmental, here::here("data", "environmental.csv"))
