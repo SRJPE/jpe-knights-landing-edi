be2cae55b50ee1a7ce471e3d188855d515a470d4
liz-fw <lstebbins@flowwest.com>
2023-09-20 12:51:22 -0700

    fixes comma in release table

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -21,7 +21,8 @@ recaptures_raw <- readxl::read_xlsx(here::here("data-raw",
 release_raw <- readxl::read_xlsx(here::here("data-raw",
                                          "qry_Knights_Release_EDI.xlsx")) |>
   mutate(releaseSubSite = ifelse(releaseSubSite == "N/A", NA, releaseSubSite),
-         appliedMarkColor = ifelse(appliedMarkColor == "Not applicable (n/a)", NA, appliedMarkColor)) |>
+         appliedMarkColor = ifelse(appliedMarkColor == "Not applicable (n/a)", NA, appliedMarkColor),
+         appliedMarkPosition = str_replace(appliedMarkPosition, ",", ":")) |>
   glimpse()
 
 release_fish_raw <- readxl::read_xlsx(here::here("data-raw",
@@ -30,17 +31,17 @@ release_fish_raw <- readxl::read_xlsx(here::here("data-raw",
 
 # write clean tables ------------------------------------------------------
 
-write_csv(trap_raw, here::here("data", "trap.csv"))
-write_csv(catch_raw, here::here("data", "catch.csv"))
-write_csv(release_raw, here::here("data", "release.csv"))
-write_csv(release_fish_raw, here::here("data", "release_fish.csv"))
-write_csv(recaptures_raw, here::here("data", "recaptures.csv"))
+write_csv(trap_raw, here::here("data", "knights_trap_edi.csv"))
+write_csv(catch_raw, here::here("data", "knights_catch_edi.csv"))
+write_csv(release_raw, here::here("data", "knights_release_edi.csv"))
+write_csv(release_fish_raw, here::here("data", "knights_release_fish_edi.csv"))
+write_csv(recaptures_raw, here::here("data", "knights_recapture_edi.csv"))
 
 
 # read in clean tables ----------------------------------------------------
 
-catch <- read.csv(here::here("data", "catch.csv")) |> glimpse()
-trap <- read.csv(here::here("data", "trap.csv")) |> glimpse()
-release <- read.csv(here::here("data", "release.csv")) |> glimpse()
-recaptures <- read.csv(here::here("data", "recaptures.csv")) |> glimpse()
-release_fish <- read.csv(here::here("data", "release_fish.csv")) |> glimpse()
+catch <- read.csv(here::here("data", "knights_catch_edi.csv")) |> glimpse()
+trap <- read.csv(here::here("data", "knights_trap_edi.csv")) |> glimpse()
+release <- read.csv(here::here("data", "knights_release_edi.csv")) |> glimpse()
+recaptures <- read.csv(here::here("data", "knights_recapture_edi.csv")) |> glimpse()
+release_fish <- read.csv(here::here("data", "knights_release_fish_edi.csv")) |> glimpse()
